7403d5981d5af3af9593f6a86f40dfd65fc5ec62
Sergio Romeu <sergioromagr@gmail.com>
2024-12-17 19:08:15 +0100

    feat: Added in scope ips as optional parameters

--- app/Console/Commands/InitProgram.php
+++ app/Console/Commands/InitProgram.php
@@ -20,6 +20,7 @@ class InitProgram extends Command
                             {name : The name of the program.} 
                             {wildcards : List of wildcards separated by commas (ej. *.example.com,*.test.com)}
                             {out_of_scope : List of wildcards or terms out of scope separated by commas (ej. *.incamail-dev.com,*.test.com, test)}
+                            {in_scope_ips? : List of range of ips in scope separated by commas (ej. 192.168.1.1, 192.168.255.255)}
                             {description? : Optional description of the program.}';
 
     /**
@@ -70,6 +71,23 @@ class InitProgram extends Command
             ]);
             $this->info("Assigning out_of_scope {$out_of_scope_item}");
          }
+
+        // Create the in_scope_ips
+        if ($this->argument('in_scope_ips')) {
+            $in_scope_ips = explode(',', $this->argument('in_scope_ips'));
+            if (count($in_scope_ips) % 2 != 0) {
+                $this->error('The list of in_scope_ips has to be a list of ranges of IPs, for example: 192.168.1.1, 192.168.255.255');
+                return Command::FAILURE;
+            }
+            for ($i = 0; $i < count($in_scope_ips); $i+=2) {
+                InScopeIp::create([
+                    'program_id' => $program->id,
+                    'ip_start' => inet_pton($in_scope_ips[$i]),
+                    'ip_end' => inet_pton($in_scope_ips[$i+1]),
+                ]);
+                $this->info("Assigning in_scope_ip range {$in_scope_ips[$i]} to {$in_scope_ips[$i+1]}");
+            }
+        }
 
         return Command::SUCCESS;
     }
